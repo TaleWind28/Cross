307b77196d8cfc7b93d3853f61886469a9b4e7f3
TaleWind28 <106275354+TaleWind28@users.noreply.github.com>
2025-02-05 10:16:01 +0100

    Provo a Togliere Warning

--- src/main/java/JsonMemories/Orderbook.java
+++ src/main/java/JsonMemories/Orderbook.java
@@ -25,6 +25,7 @@ public class Orderbook implements JsonAccessedData{
         this.askOrders = new TreeMap<>(new PriceComparator());
         this.bidOrders = new TreeMap<>(new PriceComparator().reversed());
         this.stopOrders = new ConcurrentLinkedQueue<>(); 
+        System.out.println(this.stopOrders);
     }
     
     @Override
